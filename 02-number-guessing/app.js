a9776c456caa0599c6c73c412e72daf2007c25be
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-22 19:34:10 +0000

    done

---
+++ 02-number-guessing/app.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// CGN = Computer Generated Number
+const CGN = Math.floor(Math.random() * 10 + 1);
+// User Guessing Number
+const userGuess = await inquirer.prompt([
+    {
+        'name': 'number',
+        'type': 'number',
+        'message': 'Guess the Number 1 - 10: '
+    }
+]);
+(userGuess.number === CGN) ?
+    console.log(`Your guessing number is right! ${userGuess.number}`) :
+    console.log(`Your guessing is wrong! right number is :${CGN}`);
