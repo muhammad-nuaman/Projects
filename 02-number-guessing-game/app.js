8d3da663cdd7358b9a38092af28613f578841ccf
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-26 09:02:05 +0000

    done

--- 02-number-guessing-game/app.js
+++ 02-number-guessing-game/app.js
@@ -1,15 +1,32 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-// CGN = Computer Generated Number
-const CGN = Math.floor(Math.random() * 10 + 1);
-// User Guessing Number
-const userGuess = await inquirer.prompt([
-    {
-        'name': 'number',
-        'type': 'number',
-        'message': 'Guess the Number 1 - 10: '
-    }
-]);
-(userGuess.number === CGN) ?
-    console.log(`Your guessing number is right! ${userGuess.number}`) :
-    console.log(`Your guessing is wrong! right number is :${CGN}`);
+import chalk from 'chalk';
+async function game() {
+    // CGN = Computer Generated Number
+    const CGN = Math.floor(Math.random() * 10 + 1);
+    (async function guessing() {
+        // User Guessing Number
+        const userGuess = await inquirer.prompt([
+            {
+                'name': 'number',
+                'type': 'number',
+                'message': `Guess the Number : [1-10]`
+            }
+        ]);
+        if (userGuess.number < 1 ||
+            userGuess.number > 10 ||
+            isNaN(userGuess.number)) {
+            guessing();
+        }
+        else {
+            if (userGuess.number === CGN) {
+                console.log(`${chalk.green('Congratulation')}! You've guessed it's right: ${chalk.green(userGuess.number)} `);
+            }
+            else {
+                console.log(`Wrong Guessing: ${chalk.red(userGuess.number)}\nRight Number is: ${chalk.green(CGN)}`);
+            }
+        }
+    })();
+}
+game();
+export default game;
