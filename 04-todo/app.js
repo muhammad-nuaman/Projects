2f83ddec3cc1364de2b643f386776366dbdf1b05
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-04-02 20:17:48 +0000

    done

--- 04-todo/app.js
+++ 04-todo/app.js
@@ -1,13 +1,9 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-
-
 let todos = [];
-let repeatProgram = false; 
-let askAgain = false; 
-
-
+let repeatProgram = false;
+let askAgain = false;
 do {
     // Inquiry To Add Task
     do {
@@ -16,21 +12,23 @@ do {
             type: 'input',
             message: 'Add Task: '
         });
-        if (!todo.task.length) askAgain = true;
-        else {todos.push(todo.task); askAgain = false};
-    }
-    while(askAgain)
-    
+        if (!todo.task.length)
+            askAgain = true;
+        else {
+            todos.push(todo.task);
+            askAgain = false;
+        }
+        ;
+    } while (askAgain);
     // Inquiry To Reapeat Program
     const todo = await inquirer.prompt({
         name: 'Option',
         type: 'list',
-        choices: ['More Task', 'Exit'],        
+        choices: ['More Task', 'Exit'],
     });
-    if (todo.Option === 'More Task' ) repeatProgram = true;
-    else if (todo.Option === 'Exit' ) repeatProgram = false;
-
-}
-while(repeatProgram);
-
+    if (todo.Option === 'More Task')
+        repeatProgram = true;
+    else if (todo.Option === 'Exit')
+        repeatProgram = false;
+} while (repeatProgram);
 todos.forEach((task, index) => console.log(`${chalk.green(++index)} ${task}`));
